def808ef5a8705d355b034f1c522e10a307989c0
Christabel Nana Darko <darkochristabelnana@gmail.com>
2025-06-10 12:35:09 +0000

    add: program

---
+++ main.py
@@ -0,0 +1,9 @@
+traffic_sign = ['red', 'green', 'green', 'red', 'green', 'green', 'red']
+
+
+for sign in traffic_sign:
+    if sign == 'red':
+        print("Right")
+    elif sign == 'green':
+        print("Left")
+
